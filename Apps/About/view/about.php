5a3a280b24992818b054229ecd0081ff546b2af6
IgorOGuimaraes <igor.guimaraes01@fatec.sp.gov.br>
2019-03-05 18:46:00 -0300

    Pagina sobre o projeto atualizada com a introdução

--- Apps/About/view/about.php
+++ Apps/About/view/about.php
@@ -23,7 +23,7 @@
 <nav>
     <div class="nav-wrapper red darken-4">
         <div class="container">
-            <a href="#" class="brand-logo">Gabarit.IO</a>
+            <a href="/<?=APPLICATION_NAME?>/Login/home" class="brand-logo">Gabarit.IO</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="/<?=APPLICATION_NAME?>/About/home">Guide</a></li>
                 <li><a href="/<?=APPLICATION_NAME?>/Login/home">Login</a></li>
@@ -38,14 +38,23 @@
             <div class="col s12 m12 l12">
                 <h5 class="header">About</h5>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porta accumsan odio at vestibulum. Donec placerat id lacus non porttitor. Quisque iaculis eros quis egestas dignissim. Quisque fermentum urna tellus, id eleifend velit eleifend vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin lobortis condimentum nulla, et vestibulum justo porta ut. Fusce pulvinar ligula id posuere sollicitudin. Maecenas quis quam et eros porttitor lobortis. Vivamus ut nisi nec purus mattis scelerisque vitae vitae mauris. In malesuada tincidunt nisl sed elementum. Etiam eu scelerisque nibh. Cras sollicitudin porta mi, ac tempor neque condimentum ut. Aliquam mollis odio vitae sem maximus, vitae convallis quam venenatis. Sed id nulla est. Ut sollicitudin cursus euismod. Cras faucibus magna sagittis pulvinar tincidunt.</p>
+                <p>A evolução contínua das tecnologias da informação vem promovendo diversas mudanças na sociedade como um todo. Uma das mudanças com maior impacto foi à introdução de dispositivos móveis e computadores com capacidades de processamento maiores e de tamanho excepcionalmente menor a cada geração. Atualmente, esses dispositivos são vistos como empecilhos e muitas vezes atrapalham o desenvolvimento das atividades em classe, porém, se utilizados com propósitos definidos, podem fornecer uma poderosa ferramenta de aprendizado.</p>
+                <p>Buscando intervir na integração do ensino com as novas tecnologias, este projeto visa o desenvolvimento de uma plataforma integrada Web e Mobile que auxilie o docente na correção de provas e no gerenciamento de notas e progresso dos alunos. Com foco nas avaliações objetivas, ou seja, as provas compostas por questões de múltipla escolhe e/ou verdadeiro ou falso, essa ferramenta apoiará o professor na automatização do processo de correção.</p>
+                <p>Levando em consideração o tempo que o professor gasta realizando esse processo, que é repetitivo e demanda um alto nível de atenção, essa plataforma trará benefícios para o docente, que terá mais tempo para outras atividades, pessoais e acadêmicas, assim como para os discentes, que terão suas notas atribuídas mais rapidamente e com maior grau de precisão.</p>
+                <p>A utilização planejada da ferramenta será conforme o seguinte procedimento: o docente utilizará o aplicativo móvel para leitura do gabarito da prova através da câmera do dispositivo. Os valores lidos são corrigidos pelo sistema integrado de Inteligência Artificial e então enviados para a plataforma Web. Essa plataforma poderá ser acessada tanto pelo dispositivo móvel quanto por um computador e nela será possível visualizar o desempenho do aluno através de gráficos e relatórios. </p>
+                <p>O diferencial dessa plataforma está no uso de inteligência artificial para correção das provas. Além disso, o fato de ser integrado e disponibilizado via web, possibilita que o professor tenha acesso a dados estatísticos sobre sua turma e acompanhe o desenvolvimento dos alunos durante o semestre, podendo analisar resultados ao longo do tempo.</p>
+                <p>Esta monografia compõe-se das seguintes seções: Capítulo 1 – Fundamentação Teórica, com as discussões dos autores em que se baseia o projeto sobre inteligência artificial, Android®®, tecnologias que serão utilizadas no desenvolvimento da aplicação WEB, como: HTML, JavaScript, PHP, CSS, e Banco de dados; Capítulo 2 – Metodologia, com as discussões relativas ao planejamento do trabalho; Capítulo 3 – Desenvolvimento, apresentando o passo a passo da sua consecução e os resultados; e finalmente, as Considerações Finais, com as discussões decorrentes e sugestões para futuras pesquisas.</p>
+
+<!--                <hr><br>-->
+<!---->
+<!--                <h5 class="header">Members</h5>-->
             </div>
         </div>
     </div>
     </div>
 </main>
 
-<footer class="page-footer red darken-4" style="bottom: 0; position: absolute; width: 100%;">
+<footer class="page-footer red darken-4" style="bottom: 0; position: relative; width: 100%;">
     <div class="footer-copyright">
         <div class="container center">
             Copyright © 2018 - FATEC SBC | Faculdade de Tecnologia
